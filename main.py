8ee1e0f0de86d8bff2f8fd5913768a4e7b6017d5
nikita_khala <nikita.khala09052007@gmail.com>
2023-02-23 22:10:17 +0300

    main update v2, hi func

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+def print_hi():
+    print('''
+     ##     ## ########  ######   ######  
+     ###   ###    ##    ##    ## ##    ## 
+     #### ####    ##    ##       ##       
+     ## ### ##    ##    ##        ######  
+     ##     ##    ##    ##             ## 
+     ##     ##    ##    ##    ## ##    ## 
+     ##     ##    ##     ######   ######  
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    |------------Coded by ShLk------------|''')
 
+    print("\nGithub: https://github.com/ShLkprod/MTCS_V1\n")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
