95f0d3dff0fc747439b3329922904e2ea61bc1b0
nikita_khala <nikita.khala09052007@gmail.com>
2023-02-28 22:14:21 +0300

    main update v2

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ def main_menu():
  ██║ ╚═╝ ██║   ██║   ╚██████╗███████║
  ╚═╝     ╚═╝   ╚═╝    ╚═════╝╚══════╝
 *-----------Coded by ShLk------------*''')
-
-print("\nGithub: https://github.com/ShLkprod/MTCS_V1\n")
-print("Telegram: @Laineekz and @SharpCD\n")
+    print("\nGithub: https://github.com/ShLkprod/MTCS_V1\n")
+    print("Telegram: @Laineekz and @SharpCD\n")
 
 if __name__ == '__main__':
     main_menu()
