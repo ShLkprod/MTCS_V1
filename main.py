6d332be531f3966292d64313a6571d42cd51082c
nikita_khala <nikita.khala09052007@gmail.com>
2023-02-28 23:57:51 +0300

    main_menu update clear screen upload

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import subprocess, platform
 def main_menu():
-    print('''
+    print('''\033[32m
  ███╗   ███╗████████╗ ██████╗███████╗
  ████╗ ████║╚══██╔══╝██╔════╝██╔════╝
  ██╔████╔██║   ██║   ██║     ███████╗
  ██║╚██╔╝██║   ██║   ██║     ╚════██║
  ██║ ╚═╝ ██║   ██║   ╚██████╗███████║
- ╚═╝     ╚═╝   ╚═╝    ╚═════╝╚══════╝
-*-----------Coded by ShLk------------*''')
-    print("\nGithub: https://github.com/ShLkprod/MTCS_V1\n")
-    print("Telegram: @Laineekz and @SharpCD\n")
+ ╚═╝     ╚═╝   ╚═╝    ╚═════╝╚══════╝\033[0m
+*-----------\033[31mCoded by ShLk\033[0m------------*''')
+    print("\nGithub: \033[33mhttps://github.com/ShLkprod/MTCS_V1\033[0m")
+    print("Telegram: \033[33m@Laineekz\033[0m and \033[33m@SharpCD\n")
+    input("\033[32mPress Enter to continue...\033[0m")
+def clear_screen():
+    if platform.system() == "Windows":
+        if platform.release() in {"10", "11"}:
+            subprocess.run("", shell=True)
+            print("\033c", end="")
+        else:
+            subprocess.run(["cls"])
+    else:  # Linux and Mac
+        print("\033c\033[3J")
+
 
 if __name__ == '__main__':
     main_menu()
